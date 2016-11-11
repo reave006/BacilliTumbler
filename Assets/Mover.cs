46980ce6cfce9241f03d87730a841832d6b915a5
Joshua Reaves <reave006@cougars.csusm.edu>
2016-11-11 15:36:17 -0800

    Bacilli Tumbler in microbio initialization of work already done.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,21 +6,34 @@ public class Mover : MonoBehaviour {
 	public float rotateSpeed =10.0f;
 	public float movementSpeed=5.0f;
 	private Rigidbody self;
+	private bool runOrTumble;
 
 	// Use this for initialization
 	void Start () {
 		self = GetComponent<Rigidbody> ();
+		transform.gameObject.SetActive (true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey("r")) {
-			self.AddForce ( transform.up* Time.deltaTime * movementSpeed);
+			runOrTumble = true;
+			//self.AddForce ( transform.up* Time.deltaTime * movementSpeed);
 			//transform.position += transform.forward * Time.deltaTime * movementSpeed;
 	}
 		if (Input.GetKey ("t")) {
+			runOrTumble = false;
 			//Vector3 rotateDirction = new Vector3 (0, rotateSpeed, 0);
-			transform.Rotate(Vector3.up*rotateSpeed, Space.World);
+			//transform.Rotate(Vector3.up*rotateSpeed, Space.World);
+			//self.AddRelativeTorque(Vector3.right*rotateSpeed);
+			//self.AddTorque(Vector3.up*rotateSpeed,ForceMode.Force);
+			//self.AddTorque(transform.right * rotateSpeed);
+		}
+
+		if (runOrTumble == true) {
+			self.AddForce (transform.up * Time.deltaTime * movementSpeed);
+		} else {
+			self.AddRelativeTorque (Vector3.right * rotateSpeed);
 		}
 
 		self.position = new Vector3 (
@@ -28,6 +41,7 @@ public class Mover : MonoBehaviour {
 			0.7f,
 			self.position.z
 		);
+
 
 	}
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,19 +5,22 @@ public class Rotate : MonoBehaviour {
 	public float speed=1.0f;
 	public bool rotateDirection=true;
 	private Vector3 direction;
+	private Rigidbody self;
 	// Use this for initialization
 	void Start () {
-
+		self = transform.GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (rotateDirection == true) {
-			direction = Vector3.right;
+			direction = Vector3.up;
 		} else {
 			direction = -Vector3.right;
 		}
-			
-		transform.RotateAround(transform.position, direction, Time.deltaTime * speed);
+		transform.Rotate (0, speed / Time.deltaTime, 0);
+		//transform.Rotate (direction, Space.Self);	
+		//self.AddRelativeTorque (direction, ForceMode.Force);
+		//transform.RotateAround(transform.position, direction, Time.deltaTime * speed);
 	}
 }
