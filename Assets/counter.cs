41e8fe6e90b83f979c075c620213a578fec0e4a6
Joshua Reaves <reave006@cougars.csusm.edu>
2016-11-14 18:51:23 -0800

    Added timer

---
+++ Assets/counter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class counter : MonoBehaviour {
+
+	public int seconds;
+	private float Timer=0.0f;
+	public Text TimerBox;
+
+	// Use this for initialization
+	void Start () {
+		seconds = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Timer += Time.deltaTime;
+		seconds = Mathf.Round (Timer);
+		TimerBox.text = "" + seconds;
+	}
+}
